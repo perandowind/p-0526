b7f05a8e5862496522cbbd5ed7e4c0837c6dd943
perandowind <perandowind@gmail.com>
2026-05-26 16:41:57 +0900

    fix: secretWord 주생성자 내부로 이동

--- src/main/kotlin/com/back/HomeController.kt
+++ src/main/kotlin/com/back/HomeController.kt
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HomeController(
-    private val s3Service: S3Service
-) {
+    private val s3Service: S3Service,
+
     @Value("\${custom.secretWord}")
     private val secretWord: String = ""
+) {
+
 
     @GetMapping
     fun main(): String {
